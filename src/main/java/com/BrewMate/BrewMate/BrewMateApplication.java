7411ca5f28a8d8d29276d95918ed611ec5fd150d
Patrick Carragher <carragher@live.se>
2025-02-18 10:31:02 +0100

    first commit

---
+++ src/main/java/com/BrewMate/BrewMate/BrewMateApplication.java
@@ -0,0 +1,13 @@
+package com.BrewMate.BrewMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BrewMate/BrewMate/BrewMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrewMate.BrewMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
