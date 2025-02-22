b3411a5a1bf7432fbeb559329cc91de0291ceb9a
Patrick Carragher <carragher@live.se>
2025-02-22 16:18:34 +0100

    Undid previous commit only added a comment.

--- backend/src/main/java/com/BrewMate/BrewMate/repository/UserRepository.java
+++ backend/src/main/java/com/BrewMate/BrewMate/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.BrewMate.BrewMate.repository;
 
-import com.BrewMate.BrewMate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.BrewMate.BrewMate.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
+
+
+// undo a change
