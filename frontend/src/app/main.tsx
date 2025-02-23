5cb38e2f7bee572690d77c9f6bd540554f9e26ca
SpiZeak <max@trewhitt.se>
2025-02-23 14:11:38 +0100

    Refactor frontend styles and update dependencies for Tailwind CSS integration

--- frontend/src/app/main.tsx
+++ frontend/src/app/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import '@assets/styles/index.scss';
+import '@assets/styles/tailwind.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import store from './store';
 import AppRouter from './router';
 
--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -1,9 +1,7 @@
-import reactLogo from '@assets/images/react.svg';
-import viteLogo from '/vite.svg';
-import styles from './App.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@app/store';
 import { increment } from '@features/counter/counterSlice';
+import Button from '@mui/material/Button';
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value);
@@ -11,28 +9,18 @@ function App() {
 
   return (
     <>
+      <h1 className='text-5xl font-extrabold text-center text-blue-600 mt-8'>
+        Hello world!
+      </h1>
       <div>
-        <a href='https://vite.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className={styles.logo} alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img
-            src={reactLogo}
-            className={`${styles.logo} ${styles.react}`}
-            alt='React logo'
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={styles.card}>
-        <button onClick={() => dispatch(increment())}>count is {count}</button>
+        <Button variant='contained' onClick={() => dispatch(increment())}>
+          count is {count}
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className={styles.readTheDocs}>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
   );
 }
