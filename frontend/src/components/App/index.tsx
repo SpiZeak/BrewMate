aac6cdee11cfc509f33bf3f00e620b3f2a4557b9
SpiZeak <max@trewhitt.se>
2025-02-23 14:38:15 +0100

    Enhance App component with dark theme and Bottom Navigation; update viewport meta tag and import syntax in Tailwind CSS

--- frontend/src/components/App/index.tsx
+++ frontend/src/components/App/index.tsx
@@ -2,16 +2,34 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@app/store';
 import { increment } from '@features/counter/counterSlice';
 import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useState } from 'react';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const count = useSelector((state: RootState) => state.counter.value);
   const dispatch = useDispatch();
+  const [value, setValue] = useState('recents');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
-    <>
-      <h1 className='text-5xl font-extrabold text-center text-blue-600 mt-8'>
-        Hello world!
-      </h1>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
       <div>
         <Button variant='contained' onClick={() => dispatch(increment())}>
           count is {count}
@@ -21,7 +39,33 @@ function App() {
         </p>
       </div>
       <p>Click on the Vite and React logos to learn more</p>
-    </>
+      <BottomNavigation
+        sx={{ width: 500 }}
+        value={value}
+        onChange={handleChange}
+      >
+        <BottomNavigationAction
+          label='Recents'
+          value='recents'
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction
+          label='Favorites'
+          value='favorites'
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction
+          label='Nearby'
+          value='nearby'
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction
+          label='Folder'
+          value='folder'
+          icon={<FolderIcon />}
+        />
+      </BottomNavigation>
+    </ThemeProvider>
   );
 }
 
