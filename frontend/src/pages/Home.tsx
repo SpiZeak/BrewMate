10342b598f07c8033c26d13b4c12a7f0df307e2c
SpiZeak <max@trewhitt.se>
2025-03-02 19:53:29 +0100

    Enhance Home page layout and add navigation to authentication

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,28 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { RootState } from '@app/store';
 import Button from '@mui/material/Button';
-import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router';
 
 const Home = () => {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   return (
-    <>
-      <Typography variant='h4'>Home</Typography>
-    </>
+    <Box className='flex flex-col w-full max-w-sm mx-auto mt-8 p-4 shadow-md rounded space-y-4 MuiBox-root css-0'>
+      <Typography variant='h4'>Welcome to BrewMate</Typography>
+      <Typography variant='body1'>
+        BrewMate is a simple app that helps you keep track of your homebrewing
+        recipes. Log in to get started!
+      </Typography>
+      <Button
+        variant='outlined'
+        color='primary'
+        onClick={() => {
+          void navigate('/auth');
+        }}
+      >
+        Log In
+      </Button>
+    </Box>
   );
 };
 
